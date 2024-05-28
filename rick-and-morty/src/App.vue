03db4023e1dcb72e440033f8d4063df78e046e67
Kirill <taranyuk31@mail.ru>
2024-05-28 18:58:06 +0400

    first commit

---
+++ rick-and-morty/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div id="app">
+    <nav>   
+    </nav>
+    <router-view/>
+  </div>
+</template>
+
+<style>
+@import "assets/css/bootstar.css";
+@import "assets/css/font.css";
+</style>
